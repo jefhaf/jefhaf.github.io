619bbe8accf07ab6000ba8354708b6b7b4aa66ff
jeff <jeff@email.com>
2024-09-03 15:33:06 +0200

    profile files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log("first JS file");
+// print to console
+// window.alert("Good morning");
+// print to browser window
+// 
+let allparagraphs = document.getElementsByTagName("p");
+console.log("all paragraphs elements");
+console.log(allparagraphs)
